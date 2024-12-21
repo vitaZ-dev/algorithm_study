55ee78c7a4d527c5957903092957203c37ca32b9
황지현 <132251485+vitaZ-dev@users.noreply.github.com>
2024-12-21 22:23:41 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 47.21 ms, Memory: 32.2 MB -BaekjoonHub

--- 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.js
+++ 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.js
@@ -4,12 +4,7 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-let input = [];
-
 rl.on('line', function (line) {
-    input = line.split(' ');
-}).on('close', function () {
-    let a = Number(input[0]);
-    let b = Number(input[1]);
-    console.log(`${a} + ${b} = ${a+b}`)
-});
+    const [a, b] = line.split(' ').map(Number);
+    console.log(`${a} + ${b} = ${a+b}`);
+})
