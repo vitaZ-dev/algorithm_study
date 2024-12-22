389b4c2e248914ceedc3b4f8f16cc2dc6a211b9f
황지현 <132251485+vitaZ-dev@users.noreply.github.com>
2024-12-22 20:42:31 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,7 +1,5 @@
 function solution(num_list) {
-    const answer = [...num_list];
-    let last = num_list[num_list.length - 1];
-    let y = num_list[num_list.length - 2];
-    last > y ? answer.push(last - y) : answer.push(last*2)
-    return answer;
+    const [x, last] = num_list.slice(-2);
+    last > x ? num_list.push(last - x) : num_list.push(last*2)
+    return num_list;
 }
