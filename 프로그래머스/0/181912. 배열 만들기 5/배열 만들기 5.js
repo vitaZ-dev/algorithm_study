26394b50db8fbb765dec88947e2ce1e3f5501a2f
황지현 <132251485+vitaZ-dev@users.noreply.github.com>
2025-01-02 20:20:48 +0900

    [level 0] Title: 배열 만들기 5, Time: 2.87 ms, Memory: 41.2 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.js
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.js
@@ -1,8 +1,3 @@
 function solution(intStrs, k, s, l) {
-    var answer = [];
-    intStrs.filter(int => {
-        let num = parseInt(int.substr(s, l), 10);
-        if (num > k) answer.push(num)
-    })
-    return answer;
+    return intStrs.map(int => +int.slice(s, s+l)).filter(num => num > k)
 }
