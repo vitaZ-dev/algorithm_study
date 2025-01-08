202170e0ef74d25117e15b90903f44237dd25183
황지현 <132251485+vitaZ-dev@users.noreply.github.com>
2025-01-08 21:27:59 +0900

    [level 0] Title: 수 조작하기 2, Time: 10.99 ms, Memory: 41.8 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
@@ -1,15 +1,11 @@
 function solution(numLog) {
-    var answer = '';
     const calculator = {
         "1": "w",
         "-1": "s",
         "10": "d",
         "-10": "a",
     }
-    
-    Array(numLog.length-1).fill('').forEach((_, idx) => {
-        answer += calculator[(numLog[idx+1] - numLog[idx]) + '']
-    })
-    
-    return answer;
+     return numLog.slice(1).map((num, idx) => {
+        return calculator[num - numLog[idx]]
+    }).join('');
 }
