cbc144f0151824e340a4a6e20937102290d4225f
황지현 <132251485+vitaZ-dev@users.noreply.github.com>
2025-01-08 20:45:17 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.js
@@ -1,5 +1,4 @@
 function solution(my_string, s, e) {
-    var answer = '';
     let first = my_string.slice(0, s);
     let rev = my_string.slice(s, e+1).split('').reverse().join('');
     let last = my_string.slice(e+1);
