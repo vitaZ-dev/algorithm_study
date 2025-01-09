69d27833c5bad9214882c023474bb41ba806cbb9
황지현 <132251485+vitaZ-dev@users.noreply.github.com>
2025-01-09 20:16:45 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 19.39 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/README.md
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/README.md
@@ -1,10 +1,10 @@
 # [level 0] 수열과 구간 쿼리 2 - 181923 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181923) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181923#) 
 
 ### 성능 요약
 
-메모리: 37 MB, 시간: 32.52 ms
+메모리: 36.9 MB, 시간: 19.39 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 09일 20:11:38
+2025년 01월 09일 20:16:43
 
 ### 문제 설명
 
--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -1,8 +1,3 @@
 function solution(arr, queries) {
-    var answer = [];
-    queries.forEach((query) => {
-        let temp = [...arr].slice(query[0], query[1]+1).sort((x,y) => x - y).filter(q => q > query[2]);
-        temp.length === 0 ? answer.push(-1) : answer.push(temp[0])
-    });
-    return answer;
+    return queries.map(([s, e, k]) => arr.slice(s, e+1).filter(n => n > k).sort((x, y) => x - y)[0] || -1);
 }
