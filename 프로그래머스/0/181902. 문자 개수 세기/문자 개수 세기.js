c0308fc9542d4cee601734fc4b84205954238e46
황지현 <132251485+vitaZ-dev@users.noreply.github.com>
2025-01-09 20:41:51 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.32 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
@@ -1,13 +1,10 @@
 function solution(my_string) {
-    var answer = Array(52).fill(0);
-    
-    [...my_string].map((str, idx) => {
-        if (str.charCodeAt() > 96) {
-            answer[str.charCodeAt() - 97+26] += 1;
-        } else {
-            answer[str.charCodeAt() - 65] += 1;
-        }
+    const al = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    const answer = Array(52).fill(0);
+
+    my_string.split("").map((n)=>{
+        answer[al.indexOf(n)] += 1;
     })
-    
+
     return answer;
 }
