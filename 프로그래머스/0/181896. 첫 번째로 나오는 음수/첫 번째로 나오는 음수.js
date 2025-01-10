f979be0c0729db1679095c0438ae90349208a7e6
황지현 <132251485+vitaZ-dev@users.noreply.github.com>
2025-01-10 16:17:16 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.62 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -1,3 +1,4 @@
 function solution(num_list) {
-    return num_list.indexOf([...num_list].filter(n => n < 0)[0]);
+    return num_list.findIndex(num => num < 0);
+    // return num_list.indexOf([...num_list].filter(n => n < 0)[0]);
 }
