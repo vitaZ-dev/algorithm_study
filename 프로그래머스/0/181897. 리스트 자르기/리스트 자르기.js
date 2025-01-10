f3bbdb15a46246b874897660635e9d0b76a81947
황지현 <132251485+vitaZ-dev@users.noreply.github.com>
2025-01-10 16:08:24 +0900

    [level 0] Title: 리스트 자르기, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
@@ -1,18 +1,12 @@
 function solution(n, [a, b, c], num_list) {
-    var answer = [];
     switch(n) {
         case 1:
-            answer = num_list.slice(0, b+1);
-            break;
+            return num_list.slice(0, b+1);
         case 2:
-            answer = num_list.slice(a);
-            break;
+            return num_list.slice(a);
         case 3:
-            answer = num_list.slice(a, b+1);
-            break;
+            return num_list.slice(a, b+1);
         case 4:
-            answer = num_list.slice(a, b+1).filter((_, i) => i % c === 0);
-            break;
+            return num_list.slice(a, b+1).filter((_, i) => i % c === 0);
     }
-    return answer;
 }
