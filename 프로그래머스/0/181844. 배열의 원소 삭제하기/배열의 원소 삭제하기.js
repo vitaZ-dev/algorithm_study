bf2d74e2b431687866ca1a026c88879ff4cbf2ad
황지현 <132251485+vitaZ-dev@users.noreply.github.com>
2025-01-10 16:55:04 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.js
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.js
@@ -1,6 +1,3 @@
 function solution(arr, delete_list) {
-    [...arr].forEach((num, idx) => {
-        if (delete_list.includes(num)) arr[idx] = -1;
-    });
-    return arr.filter(n => n !== -1);
+    return arr.filter(n => !delete_list.includes(n));
 }
