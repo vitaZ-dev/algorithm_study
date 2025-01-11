48712771631330db15addaf078aea6288e5e9769
황지현 <132251485+vitaZ-dev@users.noreply.github.com>
2025-01-11 13:44:40 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.js
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.js
@@ -1,8 +1,11 @@
 function solution(str_list, ex) {
+    return str_list.filter(str => !str.includes(ex)).join('');
+    /*
     return str_list.reduce((a, c) => {
         if (c.includes(ex)) return a;
         return a + c
     }, "");
+    */
     /*
     var answer = '';
     str_list.forEach(str => {
