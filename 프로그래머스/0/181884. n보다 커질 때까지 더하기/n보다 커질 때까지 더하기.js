30d66a5d4becee5e232d9618b6149107249017a0
황지현 <132251485+vitaZ-dev@users.noreply.github.com>
2025-01-12 20:11:49 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -1,8 +1,3 @@
 function solution(numbers, n) {
-    let answer = 0;
-    let i = 0;
-    while(answer <= n) {
-        answer += numbers[i++];
-    }
-    return answer;
+    return numbers.reduce((a, c) => a <=n ? a + c : a);
 }
