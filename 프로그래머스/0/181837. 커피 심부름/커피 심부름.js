845f296aa2b03613dd20fcfab70d71d1a48309c7
황지현 <132251485+vitaZ-dev@users.noreply.github.com>
2025-01-12 19:36:52 +0900

    [level 0] Title: 커피 심부름, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.js
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.js
@@ -1,8 +1,7 @@
 function solution(order) {
     var answer = 0;
     order.forEach(od => {
-        if (od.includes('americano')) answer += 4500;
-        else if (od.includes('cafelatte')) answer += 5000;
+        if (od.includes('cafelatte')) answer += 5000;
         else answer += 4500;
     })
     return answer;
