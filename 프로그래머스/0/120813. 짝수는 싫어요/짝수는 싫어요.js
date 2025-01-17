cd5bfbd68a13fa7e65eb0c9d996e69016848ed68
황지현 <132251485+vitaZ-dev@users.noreply.github.com>
2025-01-17 20:23:44 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.04 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,4 +1,7 @@
 function solution(n) {
-    return Array(Math.ceil(n / 2)).fill(1).map((num, idx) => num + 2 * idx);
+    var answer = [];
+    for (let i = 1; i <= n; i += 2) answer.push(i)
+    return answer;
+    // return Array(Math.ceil(n / 2)).fill(1).map((num, idx) => num + 2 * idx);
     // return Array(Math.ceil(n / 2)).fill(1).reduce((a, c, i) => [...a, c + 2*i], []);
 }
