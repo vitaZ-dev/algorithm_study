6b8506339db0cfd3c8c1c17041b6d98694a3a159
황지현 <132251485+vitaZ-dev@users.noreply.github.com>
2025-01-17 20:40:42 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.18 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -1,3 +1,3 @@
 function solution(my_string) {
-    return [...my_string.replace(/[a-zA-Z]/g, '')].reduce((a, c) => a + +c, 0);
+    return [...my_string.replace(/[^\d]/g, '')].reduce((a, c) => a + +c, 0);
 }
