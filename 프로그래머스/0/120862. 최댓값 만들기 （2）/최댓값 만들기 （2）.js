fb1a5a0fc5f2c3bdd9f26d683d6bbb5ccd4d1642
황지현 <132251485+vitaZ-dev@users.noreply.github.com>
2025-01-17 20:50:11 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.07 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,4 +1,4 @@
 function solution(numbers) {
     const arr = numbers.sort((x, y) => x - y);
-    return Math.max(arr[0]*arr[1], arr[arr.length-1]*arr[arr.length-2]);
+    return Math.max(arr[0] * arr[1], arr.at(-1) * arr.at(-2));
 }
