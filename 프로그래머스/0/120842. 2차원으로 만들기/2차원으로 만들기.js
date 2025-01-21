a02bcc0679039a76115bea62bacc01ebd7db58ea
황지현 <132251485+vitaZ-dev@users.noreply.github.com>
2025-01-21 14:29:53 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,9 +1,7 @@
 function solution(num_list, n) {
     const answer = [];
-    for(let i = 0; i < num_list.length / n; i++) {
-        const arr = [];
-        for(let k = 0; k < n; k++) arr.push(num_list[i*n + k])
-        answer.push(arr);
+    while (num_list.length) {
+        answer.push(num_list.splice(0, n))
     }
     return answer;
 }
