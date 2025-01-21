dcc4fe8699f39c6257914b7e0b628425c41f96c1
황지현 <132251485+vitaZ-dev@users.noreply.github.com>
2025-01-21 13:38:51 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 1.01 ms, Memory: 35.7 MB -BaekjoonHub

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -1,10 +1,3 @@
 function solution(arr) {
-    for(let i = 0; i < arr.length; i++) {
-        for(let j = 0; j < arr.length; j++) {
-            if (arr[i][j] !== arr[j][i]) {
-                return 0;
-            }
-        }
-    }
-    return 1;
+    return arr.every((a, i) => a.every((_, j) => arr[i][j] === arr[j][i])) ? 1 : 0;
 }
