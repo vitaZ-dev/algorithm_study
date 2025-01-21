c0869e712b2df23f7d65eb5585dfceae7a4f1c65
황지현 <132251485+vitaZ-dev@users.noreply.github.com>
2025-01-21 14:12:24 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 2.97 ms, Memory: 35.7 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
@@ -1,6 +1,7 @@
 function solution(arr, queries) {
     queries.forEach(([s, e]) => {
-        for(let i = s; i <= e; i++) arr[i]++
+        // for(let i = s; i <= e; i++) arr[i]++
+        while (s <= e) arr[s++]++
     })
     return arr;
 }
