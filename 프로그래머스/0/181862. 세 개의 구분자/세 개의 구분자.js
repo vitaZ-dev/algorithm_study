74f734e85cd14bf434acdab4d69f6410f61281e9
황지현 <132251485+vitaZ-dev@users.noreply.github.com>
2025-01-22 21:23:46 +0900

    [level 0] Title: 세 개의 구분자, Time: 33.54 ms, Memory: 60.8 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,4 +1,4 @@
 function solution(myStr) {
-    const answer =  myStr.replace(/[a-c]/g, ' ').split(' ').filter(s => s);
+    const answer =  myStr.split(/[a | b | c]/).filter(s => s);
     return answer.length === 0 ? ["EMPTY"] : answer;
 }
