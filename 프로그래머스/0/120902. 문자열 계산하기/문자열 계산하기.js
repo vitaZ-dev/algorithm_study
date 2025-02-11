952250a1a89917dc7ff6d2a7d30dadad65ea237f
황지현 <132251485+vitaZ-dev@users.noreply.github.com>
2025-02-11 17:58:52 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -2,12 +2,20 @@ function solution(my_string)
 {
     // 연산자가 하나만 있다는 보장이 없음!
     let op = '+';
-    let answer = 0;
-    my_string.split(' ').forEach(s => {
-        if (Number.isInteger(+s)) {
-            op === '+' ? answer += +s : answer -= +s;
+    // let answer = 0;
+    // my_string.split(' ').forEach(s => {
+    //     if (Number.isInteger(+s)) {
+    //         op === '+' ? answer += +s : answer -= +s;
+    //     }
+    //     else op = s;
+    // })
+    
+    return my_string.split(' ').reduce((a, c) => {
+        if (Number.isInteger(+c)) {
+            op === '+' ? a += +c : a -= +c;
         }
-        else op = s;
-    })
-    return answer;
+        else op = c;
+    
+        return a;
+    }, 0);
 }
