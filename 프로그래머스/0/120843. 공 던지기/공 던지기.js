4a27273645f1422a8f0fa632a71f7666dc05331f
황지현 <132251485+vitaZ-dev@users.noreply.github.com>
2025-02-11 22:13:33 +0900

    [level 0] Title: 공 던지기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.js
@@ -1,10 +1,3 @@
 function solution(numbers, k) {
-    let count = 1;
-    let answer = 0;
-    for(let i = 0; i < k; i++) {
-        answer = count;
-        count += 2;
-        if (count > numbers.length) count = count - numbers.length;
-    }
-    return answer;
+    return numbers[(2 * (k - 1)) % numbers.length];
 }
