1465036221693cc10c7a4a8ce9ccb915266d5214
황지현 <132251485+vitaZ-dev@users.noreply.github.com>
2025-02-14 12:58:22 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -1,7 +1,7 @@
 function solution(my_str, n) {
     const answer = [];
-    for(let i = 0; i < Math.ceil(my_str.length/n); i++) {
-        answer.push(my_str.substring(n*i, n*(i+1)))
+    for(let i = 0; i < my_str.length; i += n) {
+        answer.push(my_str.substring(i, i + n))
     }
     return answer;
 }
