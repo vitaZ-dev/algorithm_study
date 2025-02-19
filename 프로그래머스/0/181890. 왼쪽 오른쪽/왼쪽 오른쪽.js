b9df05acee6db0715d3d86d1e43f8407ad214035
황지현 <132251485+vitaZ-dev@users.noreply.github.com>
2025-02-19 17:35:21 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -1,16 +1,7 @@
 function solution(str_list) {
-    if (str_list.includes('l') || str_list.includes('r')) {
-        const left = str_list.indexOf('l');
-        const right = str_list.indexOf('r');
-        
-        if (left === -1) return str_list.slice(right+1)
-        if (right === -1) return str_list.slice(0, left)
-        
-        if (left < right) {
-            return str_list.slice(0, left)
-        } else {
-            return str_list.slice(right+1)
-        }
+    for(let i = 0; i < str_list.length; i++) {
+        if (str_list[i] === 'l') return str_list.slice(0, i);
+        if (str_list[i] === 'r') return str_list.slice(i + 1)
     }
     return [];
 }
