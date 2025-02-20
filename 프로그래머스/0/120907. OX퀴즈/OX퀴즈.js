db377c6fe52e047c101c0b152e9f27cb6cdbac9e
황지현 <132251485+vitaZ-dev@users.noreply.github.com>
2025-02-20 18:21:55 +0900

    [level 0] Title: OX퀴즈, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.js
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.js
@@ -3,13 +3,9 @@ function solution(quiz) {
         '+': (a, b) => +a + +b,
         '-': (a, b) => +a - +b,
     }
-    const cal = (str, res) => {
-        const [x, op, y] = str.split(' ');
-        return OP[op](x, y) === res ? 'O' : 'X';
-    }
 
     return quiz.map(q => {
-        const [str, res] = q.split(' = ');
-        return cal(str, +res);
+        const [x, op, y, _, res] = q.split(' ');
+        return OP[op](x, y) === +res ? 'O' : 'X';
     });
 }
