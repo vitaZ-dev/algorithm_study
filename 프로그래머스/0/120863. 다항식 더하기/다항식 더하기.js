450429bea25cdb1abdbbfb3af85130b1a60b9dc5
황지현 <132251485+vitaZ-dev@users.noreply.github.com>
2025-02-20 18:04:29 +0900

    [level 0] Title: 다항식 더하기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
@@ -4,9 +4,18 @@ function solution(polynomial) {
         else a[1] += +c;
         return a;
     }, [0, 0]);
+    const answer = [];
+    if(res[0]) answer.push(`${res[0] === 1 ? '' : res[0]}x`);
+    if(res[1]) answer.push(res[1]);
+
+    return answer.join(' + ');
+
+    /*
     if (res[0] === 1) res[0] = '';
 
-    if (res[1] === 0) return `${res[0]}x`;
-    else if (res[0] === 0) return `${res[1]}`
+    if (!res[0]) return `${res[1]}`;
+    if (!res[1]) return `${res[0]}x`;
+
     return `${res[0]}x + ${res[1]}`;
+    */
 }
