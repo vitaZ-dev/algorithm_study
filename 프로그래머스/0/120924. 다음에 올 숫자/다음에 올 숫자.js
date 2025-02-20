2ab4ce9ffd9370ed53d7ba99f0763608c6650cc8
황지현 <132251485+vitaZ-dev@users.noreply.github.com>
2025-02-20 18:09:05 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -1,5 +1,5 @@
 function solution(common) {
     const [a, b, c] = common;
-    if (b - a === c - b) return common.at(-1) + (b - a);
-    return common.at(-1) * (b / a);
+    if (b - a === c - b) return common.pop() + (b - a);
+    return common.pop() * (b / a);
 }
